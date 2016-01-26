89a7383487610ecd74ca8be8799f330d0f81a6e8
felipecao <felipe.carvalho@gmail.com>
2016-01-26 22:34:35 +0100

    spec 'starting balance is 200 pounds' is green

--- grails-app/domain/escapayments/Account.groovy
+++ grails-app/domain/escapayments/Account.groovy
@@ -1,9 +1,12 @@
 package escapayments
 
+import org.joda.money.Money
+
 class Account {
 
     String name
     String email
+    Money balance = Money.parse("GBP 200")
 
     static constraints = {
         name blank: false
