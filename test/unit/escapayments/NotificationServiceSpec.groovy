da0abfc166a15d5d9775bd6c0e04371dc442162d
felipecao <felipe.carvalho@gmail.com>
2016-01-26 23:48:33 +0100

    added specs for NotificationServiceSpec

--- test/unit/escapayments/NotificationServiceSpec.groovy
+++ test/unit/escapayments/NotificationServiceSpec.groovy
@@ -1,5 +1,7 @@
 package escapayments
 
+import grails.plugin.mail.MailService
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,14 +9,27 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(NotificationService)
+@Mock([Account, Transaction])
 class NotificationServiceSpec extends Specification {
 
+    MailService mailServiceMock
+
     def setup() {
+        mailServiceMock = Mock()
     }
 
-    def cleanup() {
-    }
+    void "when a confirmation e-mail is sent, mailService is invoked twice, one for each account holder involved in the transaction"() {
+        given:
+        Transaction transaction = new Transaction(
+                from: TestInputs.buildFromAccount(),
+                to: TestInputs.buildToAccount(),
+                amount: TestInputs.pounds(100)
+        )
 
-    void "test something"() {
+        when:
+        service.sendConfirmationEmailToAccountHolders(transaction)
+
+        then:
+        2 * mailServiceMock.sendMail(_ as Closure)
     }
 }
