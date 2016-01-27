a1414dff788a111470e89877bcc70230174120c2
felipecao <felipe.carvalho@gmail.com>
2016-01-27 21:48:28 +0100

    changed Geb configs to use Chrome

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,12 +1,14 @@
+import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxProfile
 
 driver = {
     //set the firefox locale to 'en-us' since the tests expect english
     //see http://stackoverflow.com/questions/9822717 for more details
-    FirefoxProfile profile = new FirefoxProfile()
-    profile.setPreference("intl.accept_languages", "en-us")
-    def driverInstance = new FirefoxDriver(profile)
+//    FirefoxProfile profile = new FirefoxProfile()
+//    profile.setPreference("intl.accept_languages", "en-us")
+//    def driverInstance = new FirefoxDriver(profile)
+    def driverInstance = new ChromeDriver()
     driverInstance.manage().window().maximize()
     driverInstance
 }
