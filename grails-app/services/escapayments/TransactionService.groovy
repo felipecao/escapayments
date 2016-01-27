10d665bd67fc844cb6fc047f0ffd92a0991bf335
felipecao <felipe.carvalho@gmail.com>
2016-01-28 00:54:23 +0100

    refactored TransactionService to improve readability

--- grails-app/services/escapayments/TransactionService.groovy
+++ grails-app/services/escapayments/TransactionService.groovy
@@ -10,11 +10,7 @@ class TransactionService {
 
     Transaction transferAmountFromAccountToAnotherAccount(Money amount, Account from, Account to) {
 
-        Transaction transaction = new Transaction(
-                from: from,
-                to: to,
-                amount: amount
-        )
+        Transaction transaction = createTransaction(amount, from, to)
 
         if(!transaction.validate()){
             transaction.discard()
@@ -22,15 +18,29 @@ class TransactionService {
         }
 
         transaction.save()
+        updateBalancesInAccounts(amount, from, to)
+        notifyAccountHolders(transaction)
 
+        return transaction
+    }
+
+    private Transaction createTransaction(Money amount, Account from, Account to){
+        return new Transaction(
+                from: from,
+                to: to,
+                amount: amount
+        )
+    }
+
+    private void updateBalancesInAccounts(Money amount, Account from, Account to){
         from.decrease(amount)
         to.increase(amount)
 
         from.save()
         to.save()
+    }
 
+    private void notifyAccountHolders(Transaction transaction){
         notificationService.sendConfirmationEmailToAccountHolders(transaction)
-
-        return transaction
     }
 }
