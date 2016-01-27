b796c0a7bfb0975f321601114e1f103554e00ef5
felipecao <felipe.carvalho@gmail.com>
2016-01-27 20:26:26 +0100

    PoundsSpec is green

--- src/groovy/escapayments/Pounds.groovy
+++ src/groovy/escapayments/Pounds.groovy
@@ -1,7 +1,12 @@
 package escapayments
 
+import org.joda.money.Money
+
 /**
  * Created by felipe on 1/27/16.
  */
 class Pounds {
+    static Money amount(Integer amount) {
+        return Money.parse("GBP 200")
+    }
 }
--- test/unit/escapayments/PoundsSpec.groovy
+++ test/unit/escapayments/PoundsSpec.groovy
@@ -1,11 +1,12 @@
 package escapayments
 
 import org.joda.money.Money
+import spock.lang.Specification
 
 /**
  * Created by felipe on 1/27/16.
  */
-class PoundsSpec {
+class PoundsSpec extends Specification {
 
     def "amount returns a money instance with the specified value"(){
         expect:
