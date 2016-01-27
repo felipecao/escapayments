fe86dfeccb760e8003823dea240efbfee5aa5df4
felipecao <felipe.carvalho@gmail.com>
2016-01-27 20:00:20 +0100

    refactored code to reduce duplication

--- grails-app/services/escapayments/NotificationService.groovy
+++ grails-app/services/escapayments/NotificationService.groovy
@@ -1,6 +1,7 @@
 package escapayments
 
 import grails.transaction.Transactional
+import org.joda.money.Money
 
 @Transactional
 class NotificationService {
@@ -8,16 +9,23 @@ class NotificationService {
     def mailService
 
     void sendConfirmationEmailToAccountHolders(Transaction transaction) {
-        mailService.sendMail {
-            to transaction.to.email
-            subject "Money credited to your account"
-            body "Amount credited: ${transaction.amount.toString()}"
-        }
+        notifyCredit(transaction.to.email, transaction.amount)
+        notifyDebit(transaction.from.email, transaction.amount)
+    }
 
+    private void notifyCredit(String email, Money amount){
         mailService.sendMail {
-            to transaction.from.email
+            to email
+            subject "Money credited to your account"
+            body "Amount credited: ${amount.toString()}"
+        }
+    }
+
+    private void notifyDebit(String email, Money amount){
+        mailService.sendMail {
+            to email
             subject "Money debited to your account"
-            body "Amount debited: ${transaction.amount.toString()}"
+            body "Amount debited: ${amount.toString()}"
         }
     }
 }
